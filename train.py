4cd058408fd0695e918c25ae3eba2dfa372614c4
houston-d <dougalhouston@gmail.com>
2022-05-05 23:28:23 +0100

    Add small board options

--- train.py
+++ train.py
@@ -1,6 +1,14 @@
 import os
 from datetime import datetime
-from Runners.Ludo_Reinforce import run_game
+
+from Runners.Ludo_DQN import run_game
+
+
+# from Runners.Ludo_AC import run_game
+# from Runners.Ludo_Reinforce import run_game
+# from Runners.Ludo_DQN_Multi import run_game
+# from Runners.Ludo_AC_Multi import run_game
+# from Runners.Run_Small import run_game
 
 
 def main():
@@ -10,6 +18,14 @@ def main():
 
     returns = run_game(episodes, run_id)
 
+    string = print_full(returns, run_id)
+    # string = print_small(returns, run_id)
+
+    print("RUN %s complete" % run_id)
+    print(string)
+
+
+def print_full(returns, run_id):
     now = datetime.now()
     date_time = now.strftime("%Y%m%d_%H%M%S")
     string = '%s_%s.txt' % (date_time, run_id)
@@ -17,7 +33,6 @@ def main():
     if not os.path.exists(file_path):
         os.makedirs(file_path)
     f = open("%s%s" % (file_path, string), "a")
-
 
     f.write(','.join(str(e) for e in returns[0]))
     f.write("\n")
@@ -33,14 +48,31 @@ def main():
     # f.write("\n")
     f.flush()
     f.close()
+    return string
 
-    print("RUN %s complete" % run_id)
-    print(string)
+
+def print_small(returns, run_id):
+    now = datetime.now()
+    date_time = now.strftime("%Y%m%d_%H%M%S")
+    string = '%s_%s.txt' % (date_time, run_id)
+    file_path = "results/small/"
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    f = open("results/small/%s.txt" % string, "a")
+
+    f.write(','.join(str(e) for e in returns[0]))
+    f.write("\n")
+    f.write(','.join(str(e) for e in returns[1]))
+    f.write("\n")
+    f.write(','.join(str(e) for e in returns[2]))
+    f.write("\n")
+    f.flush()
+    f.close()
+
+    return string
+
 
 # hare run --rm -v "$(pwd)":/code --workdir /code bath:2020 python3 /code/unit_tests.py
-
-# plt.xticks(range(min(min0, min1) - 0.1, max(max0, max1) + 0.1))
-
 
 if __name__ == '__main__':
     main()
